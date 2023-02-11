56d91ca6d797c929df934c3bf8af93e3016e6fa6
Jesse Pantoja <pantojabruin@gmail.com>
2023-02-11 01:17:16 -0800

    Implement phonebook database step 3, delete requests for entries are now reflected in db

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -47,13 +47,14 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const index = phonebook.findIndex(person => person.id === id);
-
-  if (index === -1) return res.status(404).end();
-
-  phonebook.splice(index, 1);
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({'error': 'Something went wrong, couldn\'t delete'});
+    })
 })
 
 app.get('/api/persons', async (req, res) => {
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -7,11 +7,19 @@ mongoose.set('strictQuery',false)
 mongoose.set('bufferTimeoutMS', 60000);
 mongoose.connect(url).then(() => console.log('Connected to db')).catch(console.log);
 
-
 const personSchema = new mongoose.Schema({
     name: String,
     number: String,
 })
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  });
+
 
 module.exports = mongoose.model('Person', personSchema);
 
